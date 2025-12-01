fafaf04d4d1ec558497a2beb904cb1812bf347e0
tusharwashishtha2 <tusharwashishtha2@gmail.com>
2025-12-02 01:41:24 +0530

    chore: force backend restart

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const express = require('express');
+const express = require('express'); // Force restart
 const mongoose = require('mongoose');
 const cors = require('cors');
 
